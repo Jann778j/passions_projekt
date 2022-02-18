066fed64eb083683018d8757241b7d6bd755ea7f
Victor Ly <vict4222@stud.kea.dk>
2022-02-18 11:47:15 +0100

    sync

---
+++ load-data.js
@@ -0,0 +1,28 @@
+const url = "https://tema7-89db.restdb.io/rest/city";
+
+const options = {
+  headers: {
+    "x-apikey": "61fcf7e43f215f310a37be57",
+  },
+};
+
+async function getData() {
+  const response = await fetch(url, options);
+  const json = await response.json();
+  show(json);
+}
+
+const section = document.getElementById("content");
+const template = document.querySelector("template").content;
+
+function show(json) {
+  console.log(json);
+  json.forEach((city) => {
+    const clone = template.cloneNode(true);
+    clone.querySelector("img").src = `billeder/${city.billede}`;
+    clone.querySelector(".name").textContent = `${city.by}`;
+    section.appendChild(clone);
+  });
+}
+
+getData();
