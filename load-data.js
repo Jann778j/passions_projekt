32e8da68fbd823a6a2133a28e9d49d1e37da3ace
Victor Ly <vict4222@stud.kea.dk>
2022-02-21 12:48:34 +0100

    single-view

--- load-data.js
+++ load-data.js
@@ -3,45 +3,65 @@ const options = {
   headers: {
     "x-apikey": "61fcf7e43f215f310a37be57",
   },
-};
+}; //restdb.io
 
-let cities;
-let filter = "alle";
+let cities; //json databasen
+let filter = "alle"; //variabel som ændrer sig alt efter hvilken filterknap du klikker på
 
 document.addEventListener("DOMContentLoaded", () => {
+  //venter indtil siden er loadet før knapperne bliver funktionelle
   const filterButtons = document.querySelectorAll("div button");
-  filterButtons.forEach((button) =>
-    button.addEventListener("click", filterMenu)
-  );
-  fetchData();
+  filterButtons.forEach((button) => button.addEventListener("click", filterMenu)); //knapperne kalder på filterMenu() funktionen, når man klikker
+  fetchData(); //kalder på fetchData() funktionen
 });
 
 async function fetchData() {
+  //kaldes når siden er loadet
   const response = await fetch(url, options);
   cities = await response.json();
-  display(cities);
-  //console.log(json);
+  display(cities); //kalder på display() funktionen med cities som parameter
+  //console.log(cities);
 }
 
 function filterMenu() {
-  filter = this.dataset.category;
+  //bliver kaldt når knapperne klikkes på
+  filter = this.dataset.category; //variablen ændres til den knap man klikker på
   document.querySelector(".selected").classList.remove("selected");
   this.classList.add("selected");
 
-  display(cities);
+  display(cities); //kalder på display() funktionen med cities som parameter
 }
 
 function display() {
+  //kaldes når databasen er hentet
   const mainContent = document.getElementById("main_content");
   const template = document.querySelector("template").content;
-  mainContent.textContent = "";
+  mainContent.textContent = ""; //fjerner sektionens indhold
 
   cities.forEach((city) => {
     if (filter == city.kontinent || filter == "alle") {
+      //hvis objektet har samme værdi som filterknappen
       const clone = template.cloneNode(true);
       clone.querySelector("img").src = `billeder/${city.billede}`;
       clone.querySelector(".name").textContent = `${city.by}`;
+      clone.querySelector("article").addEventListener("click", () => showPopUp(city)); //gør kortene klikbart og kalder på showPopUp() funktionen med city som parameter
       mainContent.appendChild(clone);
     }
   });
-} //display all data
+}
+
+const popUp = document.getElementById("popup");
+
+function showPopUp(city) {
+  //kaldes når et kort klikkes
+  //console.log(city);
+  popUp.style.display = "block";
+  popUp.querySelector(".by").textContent = `${city.by}`;
+  popUp.querySelector(".pop_up_img").src = `billeder/${city.billede}`;
+  popUp.querySelector(".land").textContent = `Land: ${city.land}`;
+  popUp.querySelector(".beskrivelse").textContent = `${city.beskrivelse}`;
+  popUp.querySelector(".sprog").textContent = `Sprog: ${city.sprog}`;
+  popUp.querySelector(".befolkning").textContent = `${city.by} har en befolkning på ${city.befolkning} mennesker.`;
+}
+
+document.getElementById("close").addEventListener("click", () => (popUp.style.display = "none")); //lukke knap
